d1e0c0b0a61c7bea139454bb318f410a89923dbc
FBS-ENAGIC-05 <lourenisobel18@gmail.com>
2024-06-14 10:32:29 +0800

    updating version

--- src/components/helpers/functions-general.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export const urlPathSchool = "http://localhost/react-dashboard";
-export const devApiUrl = `${urlPathSchool}/rest`;
-
-export const devKey =
-  "$2a$12$47wDvbLInZif/PVS8B6P3.7WxyJvUpBzZAWCsnWJUKq3nrn4qgmeO";
